55a569d09ab20e03248fe9356f929ee5df62009c
whkoh <whkoh@users.noreply.github.com>
2021-07-10 21:36:26 +0800

    replace vals = 0

--- 2021-wk12/main.py
+++ 2021-wk12/main.py
@@ -7,9 +7,17 @@ def main():
     in2df.dropna(inplace=True)
     in2df = in2df[in2df['Hierarchy-Breakdown'].str.slice(0,40) == 'Real Sector / Tourism / Tourist arrivals']
     in2df['type'] = np.where(in2df['Hierarchy-Breakdown'] == 'Real Sector / Tourism / Tourist arrivals',
-                             'continent', 'country')
+                             'continent', 'country')  # Split workflow to country and continent
+    in2df['Country'] = np.where(in2df['Series-Measure'].str.contains('from'),
+                                in2df['Series-Measure'].str.slice(22,len(in2df['Series-Measure'])),
+                                'UN and others')
+    in2df.loc[in2df['measure'] == 'na', 'measure'] = 0
+    countrydf = in2df[in2df['type'] == 'country']
+    continentdf = in2df[in2df['type'] == 'continent']
+    countrydf['Breakdown'] = countrydf['Hierarchy-Breakdown'].str.slice(43,len(countrydf['Hierarchy-Breakdown']))
+
+
     print('test')
 
 if __name__ == '__main__':
     main()
-
