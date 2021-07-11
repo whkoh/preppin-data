0ac086b847fb3a27ce50d54f220c3c7878614163
whkoh <whkoh@users.noreply.github.com>
2021-07-11 19:59:01 +0800

    2021w24 - why isn't merge working?

--- 2021-wk24/main.py
+++ 2021-wk24/main.py
@@ -3,15 +3,24 @@ abs_file = '/Users/whkoh/PycharmProjects/preppin-data/2021-wk24/Absenteeism Scaf
 
 absence_df = pd.read_excel(abs_file, sheet_name='Reasons')
 out_df = pd.DataFrame(columns=['Name', 'Days_Absence'])
-# print(f'first out_df is {out_df}')
+
 for i in range(len(absence_df)):
-    # count_absence = absence_df.iloc[i]['Days Off']
     for j in range(absence_df.iloc[i]['Days Off']):
-        # jrow = pd.DataFrame()
-        # jrow['Name'] = absence_df.iloc[i]['Name']
-        # jrow['Days_Absence'] = absence_df.iloc[i]['Start Date'] + pd.Timedelta(days=j)
         jrow = [absence_df.iloc[i]['Name'], absence_df.iloc[i]['Start Date'] + pd.Timedelta(days=j)]
         out_df.loc[len(out_df)] = jrow
-        # print(out_df)
-    # out_df =
+
+days_out_df = out_df.groupby(['Days_Absence']).count()
+days_out_df.reset_index(inplace=True)
+
+days_df = pd.DataFrame(columns=['Days_Absence'])
+days_df.reset_index(inplace=True)
+for i in pd.date_range(start='2021-04-01', end='2021-05-31'):
+    days_df.loc[len(days_df)] = i
+
+print(days_df.dtypes)
+print(days_out_df.dtypes)
+
+final_df = days_df.merge(days_out_df, on='Days_Absence')
+final_df = final_df.fillna(0, inplace=True)
+
 print('')
