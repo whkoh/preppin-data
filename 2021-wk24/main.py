6a751152334ca846e6eebb82d99fa6ee7555e1b5
whkoh <whkoh@users.noreply.github.com>
2021-07-11 19:43:05 +0800

    2021w24 - figured out how to append using loc

--- 2021-wk24/main.py
+++ 2021-wk24/main.py
@@ -2,10 +2,16 @@ import pandas as pd
 abs_file = '/Users/whkoh/PycharmProjects/preppin-data/2021-wk24/Absenteeism Scaffold.xlsx'
 
 absence_df = pd.read_excel(abs_file, sheet_name='Reasons')
-out_df = pd.DataFrame(columns=['Name', 'DateAbs'])
+out_df = pd.DataFrame(columns=['Name', 'Days_Absence'])
+# print(f'first out_df is {out_df}')
 for i in range(len(absence_df)):
     # count_absence = absence_df.iloc[i]['Days Off']
     for j in range(absence_df.iloc[i]['Days Off']):
-        out_df.append(absence_df.iloc[i]['Name'], absence_df.iloc[i]['Start Date'] + pd.Timedelta(1, units='d'))
+        # jrow = pd.DataFrame()
+        # jrow['Name'] = absence_df.iloc[i]['Name']
+        # jrow['Days_Absence'] = absence_df.iloc[i]['Start Date'] + pd.Timedelta(days=j)
+        jrow = [absence_df.iloc[i]['Name'], absence_df.iloc[i]['Start Date'] + pd.Timedelta(days=j)]
+        out_df.loc[len(out_df)] = jrow
+        # print(out_df)
     # out_df =
 print('')
