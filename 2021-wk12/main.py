213fe553869ce1434298409f3c3d027b76a8130b
whkoh <whkoh@users.noreply.github.com>
2021-07-11 18:23:35 +0800

    done

--- 2021-wk12/main.py
+++ 2021-wk12/main.py
@@ -13,23 +13,35 @@ def main():
                                 'UN and others')
     in2df.loc[in2df['measure'] == 'na', 'measure'] = 0
     in2df['measure'] = in2df['measure'].astype(float)
-    print(in2df.dtypes)
+    # print(in2df.dtypes)
     countrydf = in2df[in2df['type'] == 'country']  # Separately analyse country and continents
     continentdf = in2df[in2df['type'] == 'continent']
     continentdf.rename(columns={'Country': 'Breakdown'}, inplace=True)
     continentdf_grp = continentdf.groupby(['yr_mo', 'Breakdown']).sum()
+    input_sum = continentdf_grp['measure'].sum()
     countrydf['Breakdown'] = countrydf['Hierarchy-Breakdown'].str.slice(43, len(countrydf['Hierarchy-Breakdown']))
     countrydf_grp = countrydf.groupby(['yr_mo', 'Breakdown']).sum()  # Use this to compare to continent
+    detail_sum = countrydf_grp['measure'].sum()
     deltas_df = pd.merge(left=continentdf_grp, right=countrydf_grp, how='outer', on=['yr_mo', 'Breakdown'])
     deltas_df.fillna(0, inplace=True)
     deltas_df.drop(columns=['id_x', 'id_y'], inplace=True)
     deltas_df['delta'] = deltas_df['measure_x'] - deltas_df['measure_y']
-    deltas_df['country'] = 'Unknown'
+    deltas_df['Country'] = 'Unknown'
     deltas_df = deltas_df[deltas_df['delta'] != 0]
     deltas_df.rename(columns={'delta': 'measure'}, inplace=True)
     deltas_df.drop(columns=['measure_x', 'measure_y'], inplace=True)
-    deltas_df.set_index(['country'], append=True, inplace=True)
-    print('test')
+    deltas_df.set_index(['Country'], append=True, inplace=True)
+    unknown_sum = deltas_df['measure'].sum()
+    compute_sum = detail_sum + unknown_sum
+    # print(f'input_sum --> {input_sum}')
+    # print(f'sums --> {compute_sum}')
+    assert input_sum == compute_sum, 'sums do not compute'  # if continent sum does not equal, we need to check
+    countrydf_spc = countrydf.groupby(['yr_mo', 'Breakdown', 'Country']).sum()
+    final_df = pd.concat([countrydf_spc, deltas_df])
+    final_sum = final_df['measure'].sum()
+    assert input_sum == final_sum, 'final sum does not compute'  # if continent sum does not equal, we need to check
+    final_df.drop(['id'], axis=1, inplace=True)
+    # print(final_df.head(20))
 
 if __name__ == '__main__':
     main()
---
+++ test/parse-xbrl.py
@@ -0,0 +1,4 @@
+from ixbrlparse import IXBRL
+
+with open('/Users/whkoh/PycharmProjects/preppin-data/test/aapl-20201226.htm', encoding='utf-8') as a:
+    x = IXBRL(a)
