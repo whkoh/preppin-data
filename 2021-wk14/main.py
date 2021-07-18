cb14e7cf5872ab1ca6878e21f15efdd4b0d4fe8d
whkoh <whkoh@users.noreply.github.com>
2021-07-18 19:50:28 +0800

    part1 done

--- 2021-wk14/main.py
+++ 2021-wk14/main.py
@@ -10,4 +10,14 @@ flights_df[['FlightID', 'Dep_Airport', 'Arr_Airport', 'Dep_Date',
 flights_df.drop(flights_df.columns[0], axis=1, inplace=True)
 flights_df.iloc[:, 0] = flights_df.iloc[:, 0].str[1:3]
 flights_df.iloc[:, 4] = flights_df.iloc[:, 4].str[0:8]
+
+labelseat_dict = [['A', 'Window'], ['F', 'Window'], ['B', 'Middle'], ['E', 'Middle'], ['C', 'Aisle'],
+                  ['D', 'Aisle']]
+labelseat_df = pd.DataFrame(labelseat_dict, columns=['RowCol', 'SeatLabel'])
+
+seatsloc_df = pd.melt(seatsloc_df, id_vars=['Row'], var_name='RowCol')
+# seatsloc_df['SeatConc'] = seatsloc_df.agg(lambda x: f"{x['RowCol']}{x['value']}", axis=1)
+seatsloc_df.rename(columns={'value': 'SeatNumber'}, inplace=True)
+seatsloc_df = seatsloc_df.merge(labelseat_df, on='RowCol', how='outer')
+
 print('ok')
